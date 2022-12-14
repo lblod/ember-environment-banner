6689e03e4cf5c311efd1f9078658ce076b7a9f20
iurianu <iulianandriescu@gmail.com>
2022-12-14 16:23:51 +0200

    increase addon options

--- addon/components/environment-banner.js
+++ addon/components/environment-banner.js
@@ -2,12 +2,36 @@ import Component from '@glimmer/component';
 import { getOwner } from '@ember/application';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' || window.location.hostname === '[::1]'
+);
+
 export default class EnvironmentBannerComponent extends Component {
   @tracked
   showModal = false;
 
   get environment() {
     return getOwner(this).resolveRegistration('config:environment');
+  }
+
+  get environmentName() {
+    const thisEnvironmentValues = isLocalhost
+    ? 'local'
+    : getOwner(this).resolveRegistration('config:environment')
+        .environmentName;
+
+    return thisEnvironmentValues;
+  }
+
+  get environmentTitle() {
+    if (environment.environmentName === 'test') {
+      return thisEnvironmentValues = 'testomgeving'
+    } else if (environment.environmentName === 'development') {
+      return thisEnvironmentValues = isLocalhost ? 'lokale omgeving' : 'ontwikkelomgeving'
+    } else {
+      return null
+    }
   }
 
   get packages() {
