5ad39059d6e1d1dcb2a755e38324fc78b1505d0f
Claire Lovisa <lovisa.claire@gmail.com>
2023-01-30 08:52:43 +0100

    Fix lint errors

--- addon/helpers/string-format.js
+++ addon/helpers/string-format.js
@@ -2,6 +2,6 @@ import { helper } from '@ember/component/helper';
 import { htmlSafe } from '@ember/template';
 
 export default helper(function StringFormat([string]) {
-    if(!string) return '';
-    return htmlSafe(string);
+  if (!string) return '';
+  return htmlSafe(string);
 });
