9a72042dd64d8815fb0fe4b745180d4295180d64
Elena Poelman <elena.poelman@redpencil.io>
2023-06-20 20:02:23 +0200

    upgrade to ember 4.12

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -15,26 +15,10 @@ module.exports = async function () {
         },
       },
       {
-        name: 'ember-release',
+        name: 'ember-lts-4.8',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('release'),
-          },
-        },
-      },
-      {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta'),
-          },
-        },
-      },
-      {
-        name: 'ember-canary',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('canary'),
+            'ember-source': '~4.8.0',
           },
         },
       },
